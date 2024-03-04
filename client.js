addf7b3b0ee5331e357309a438fb1e977ae1f36b
Jefte Franco <jeftefranco@outlook.com>
2024-03-04 13:28:08 -0600

    Bucle por stdin

--- client.js
+++ client.js
@@ -1,23 +1,39 @@
+import { stdin, exit } from "process";
 import { loadPackageDefinition, credentials } from '@grpc/grpc-js';
 import { loadSync } from '@grpc/proto-loader';
 const packageDefinition = loadSync('conversion.proto');
 const conversion_proto = loadPackageDefinition(packageDefinition).conversion;
-
+const port = '8080'
 function llamarServidorGRPC(valor, unidadOrigen, unidadDestino) {
-    const client = new conversion_proto.Conversion('localhost:50051', credentials.createInsecure());
+    const client = new conversion_proto.Conversion('localhost:8080', credentials.createInsecure());
 
     client.convertirLongitud({ valor: valor, unidadOrigen: unidadOrigen, unidadDestino: unidadDestino }, (error, response) => {
         if (error) {
             console.error('Error:', error.message);
         } else {
-            console.log(`Resultado: ${response.resultado}`);
+            console.log(`Resultado:\n${valor} ${unidadOrigen}(s) es igual a ${response.resultado} ${unidadDestino}(s)`);
         }
     });
 }
 
 // Ejemplo de uso
 const valor = 20;
-const unidadOrigen = 'milla';
-const unidadDestino = 'km';
+const unidadOrigen = 'yarda';
+const unidadDestino = 'cm';
 
-llamarServidorGRPC(valor, unidadOrigen, unidadDestino);
+console.log('Podrás convetir de pulgada, pie, yarda, milla\na centimetros, metros y kilometros\nSolo debes ingresar ');
+
+stdin.on('data', (data) => {
+    if(data.toString().trim() != 'salir'){
+        // Procesar los datos recibidos
+        let entrada = data.toString().trim().split(' ')
+        console.log('Has ingresado:', entrada);
+        console.log(entrada[0]);
+        console.log(entrada[1]);
+        console.log(entrada[2]);
+
+        llamarServidorGRPC(entrada[0], entrada[1], entrada[2]);
+    }else{
+        exit();
+    }
+  });
--- server.js
+++ server.js
@@ -1,15 +1,20 @@
+// Importamos librerias para usar RPC
 import { loadPackageDefinition, Server, ServerCredentials } from '@grpc/grpc-js';
 import { loadSync } from '@grpc/proto-loader';
+
+// Cargamos una definición del paquete gRPC como una jerarquía de objetos gRPC 
 const packageDefinition = loadSync('conversion.proto');
 const conversion_proto = loadPackageDefinition(packageDefinition).conversion;
 
+// Funcion que hace la conversion,
 function convertirLongitud(call, callback) {
     const valor = call.request.valor;
     const unidadOrigen = call.request.unidadOrigen;
     const unidadDestino = call.request.unidadDestino;
 
+    // 1 unidad y su equivalente en metros
     const factores = {
-        'pulgada': 0.0254,
+        'pulgada': 0.0254,      //1 pulgada = 0.0254 metros
         'pie': 0.3048,
         'yarda': 0.9144,
         'milla': 1609.344
@@ -36,14 +41,15 @@ function convertirLongitud(call, callback) {
     callback(null, { resultado: resultado });
 }
 
+const port = '8080'
+
 function main() {
     const server = new Server();
     server.addService(conversion_proto.Conversion.service, {
         convertirLongitud: convertirLongitud,
     });
-    server.bindAsync('0.0.0.0:50051', ServerCredentials.createInsecure(), () => {
-        console.log('Servidor gRPC en ejecución en el puerto 50051');
-        // server.start();
+    server.bindAsync(`localhost:${port}`, ServerCredentials.createInsecure(), () => {
+        console.log(`Servidor gRPC en ejecución en el puerto ${port}`);
     });
 }
 
